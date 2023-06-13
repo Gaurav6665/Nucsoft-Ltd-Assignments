ece9e27907d8400a8b40fe0d17550f7461fd286a
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:15:18 +0530

    Delete 28Top3MostPopularOrder.sql

--- 28Top3MostPopularOrder.sql
+++
@@ -1,7 +0,0 @@
-SELECT categories.category_name, COUNT(*) AS order_count
-FROM categories
-INNER JOIN products ON categories.category_id = products.category_id
-INNER JOIN order_details ON products.product_id = order_details.product_id
-GROUP BY categories.category_name
-ORDER BY order_count DESC
-LIMIT 3;
