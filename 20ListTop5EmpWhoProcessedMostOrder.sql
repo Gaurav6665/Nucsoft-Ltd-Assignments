dbbbed7d560faefec127cc01b4ecea7e67ce6200
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:13:22 +0530

    Delete 20ListTop5EmpWhoProcessedMostOrder.sql

--- 20ListTop5EmpWhoProcessedMostOrder.sql
+++
@@ -1,6 +0,0 @@
-SELECT employees.employee_id, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name, COUNT(orders.order_id) AS total_orders
-FROM employees
-INNER JOIN orders ON employees.employee_id = orders.employee_id
-GROUP BY employees.employee_id
-ORDER BY total_orders DESC
-LIMIT 5;
