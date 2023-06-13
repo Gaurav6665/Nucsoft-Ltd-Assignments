4c007aae60f03516a5c396c73faba26ee02d1270
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:10:34 +0530

    Delete 11AvgProductPriceByCategory.sql

--- 11AvgProductPriceByCategory.sql
+++
@@ -1,4 +0,0 @@
-SELECT categories.category_name, AVG(products.unit_price) AS average_price
-FROM products
-INNER JOIN categories ON products.category_id = categories.category_id
-GROUP BY categories.category_name;
