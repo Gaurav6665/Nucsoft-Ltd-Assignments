c58fc6a65cf14c62b3498d8aa87ab7ae6152313d
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:12:45 +0530

    Delete 18FindEmpReportedToAndrewFuller.sql

--- 18FindEmpReportedToAndrewFuller.sql
+++
@@ -1,4 +0,0 @@
-SELECT employees.*
-FROM employees
-INNER JOIN employees AS manager ON employees.reports_to = manager.employee_id
-WHERE manager.last_name = 'Fuller' AND manager.first_name = 'Andrew';
