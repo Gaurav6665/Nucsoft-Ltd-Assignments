84890529d4fa967e366f3b3d9586c9ebd6cd6c47
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:09:23 +0530

    Delete 10ListAllOrderCusandEmpWhoProcessed.sql

--- 10ListAllOrderCusandEmpWhoProcessed.sql
+++
@@ -1,4 +0,0 @@
-SELECT orders.*, customers.*, CONCAT(employees.first_name, ' ', employees.last_name) AS employee_name
-FROM orders
-INNER JOIN customers ON orders.customer_id = customers.customer_id
-INNER JOIN employees ON orders.employee_id = employees.employee_id;
