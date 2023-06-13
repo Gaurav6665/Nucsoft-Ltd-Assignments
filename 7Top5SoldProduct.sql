fe24c2d9e0b6fd6e24537f345f0f3d1915ca03ec
Gaurav Tiwari <68872023+Gaurav6665@users.noreply.github.com>
2023-06-13 20:18:49 +0530

    Delete 7Top5SoldProduct.sql

--- 7Top5SoldProduct.sql
+++
@@ -1,6 +0,0 @@
-SELECT products.*, SUM(order_details.quantity) AS total_sold
-FROM products
-INNER JOIN order_details ON products.product_id = order_details.product_id
-GROUP BY products.product_id
-ORDER BY total_sold DESC
-LIMIT 5;
